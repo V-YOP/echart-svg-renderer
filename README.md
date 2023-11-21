df7ec4cf5bbb559fe1dea43e79c87119601a4325
imyme <imyme@meow.me>
2023-11-21 23:26:49 +0800

    a big step

--- README.md
+++ README.md
@@ -2,23 +2,14 @@ echarts服务端渲染工具，生成SVG字符串，公司项目用。
 
 # 使用方法
 
-node版本测试时使用的是`v18.15.0`，但其他版本应该也可行?
+node版本测试时使用的是`v18.15.0`，但其他版本应该也可行。
 
-0. 执行 `npm i; npm run build` 下载依赖和编译代码到win/linux可执行文件。
+1. 执行 `npm i; npm run build` 下载依赖和编译代码到win/linux可执行文件。
+2. 编写图表对应图表定义和数据文件，参照`./dist/echart-server-renderer-win.exe --help`以及目录下的`exampleChartDef.js`和`exampleChartData.js`
+3. 执行 `./dist/echart-server-renderer-win.exe -w <图表宽度> -h <图表高度> -f <图表定义js文件名> <数据定义js文件名> > result.svg`
+4. 检查输出结果
 
-1. 编写图表对应js文件，内容如下
+考虑数据定义由后端提供，图表定义由前端编写，其对数据进行转化并构造最终的option。
 
-```javascript
-CHART_PARAM = {
-    width: 800, // SVG的宽，高
-    height: 600,
-    option: {/* 图表的option */}
-}
-```
-
-2. 将该js文件内容编码为base64，作为参数（不需要加引号）传递给可执行文件：
-
-```
-./dist/echart-server-renderer-win.exe <base64内容> > res.svg
-```
+考虑测试时使用-f传递js文件名，生产环境使用base64在后台进行调用；获取图表定义和数据定义使用了eval，因此在后台调用时应当始终从数据库拿图表定义，并手动构造数据定义。
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const echarts = require('echarts');
 // const { createCanvas } = require('canvas');
-const { writeFileSync } = require('fs');
-const { argv } = require('process');
+const { writeFileSync, readFileSync } = require('fs');
+const { program } = require('commander')
 // function renderCanvas(width, height, echartOptions) {
 //     const canvas = createCanvas(width, height);
 //     // ECharts can use the Canvas instance created by node-canvas as a container directly
@@ -32,14 +32,55 @@ function fromBase64(str) {
 function toBase64(str) {
     return Buffer.from(str).toString('base64')
 }
-if (argv.length != 3) {
-    throw new Error("a base64 must be given");
-}
-eval(fromBase64(argv[2]))
-if (!CHART_PARAM) {
-    throw new Error('variable CHART_PARAM not found')
-}
-const {width, height, option} = CHART_PARAM
 
+program
+    .description("渲染echarts图标到SVG，其中图表定义和图表数据均使用base64编码；在图表定义中可以使用echarts库中的玩意（const echarts=require('echarts')）")
+    .option('-f, --file', '允许传文件名而非base64，供测试用')
+    .option('-w, --width <pixels>', 'svg宽度', 800)
+    .option('-h, --height <pixels>', 'svg高度', 600)
+    .argument('<chartDefinition>', '必填，图表定义，为base64编码的js片段，其必须在顶层引入函数CHART_DEF，其接受单个参数data，为chartData中引入的变量 CHART_DATA')
+    .argument('[chartData]', '选填，图表所使用的数据，为base64编码的js片段，其必须在顶层引入变量CHART_DATA，不传时默认为base64("CHART_DATA={}")', toBase64('CHART_DATA={}'));
+
+program.parse();
+const {width, height, file: withFile} = program.opts()
+const [chartDefinition, chartData] = program.args
+
+if (withFile) {
+    try {
+        const chartDefStr = readFileSync(chartDefinition).toString('utf8')
+        eval(chartDefStr)
+    } catch (e) {
+        console.error(`eval 文件 ${chartDefinition} 失败，请检查其是否存在且是合法js文件`)
+        throw e
+    }
+    try {
+        const chartDataStr = readFileSync(chartData).toString('utf8')
+        eval(chartDataStr)
+    } catch (e) {
+        console.error(`eval 文件 ${chartData} 失败，请检查其是否存在且是否是合法js文件`)
+        throw e
+    }
+} else {
+    try {
+        eval(fromBase64(chartDefinition))
+    } catch (e) {
+        console.error(`解析 chartDefinition 失败，请检查其是否是使用base64编码后的合法的js片段`)
+        throw e
+    }
+    try {
+        eval(fromBase64(chartData))
+    } catch (e) {
+        console.error('解析 chartData 失败，请检查其是否是使用base64编码后的合法的js片段')
+        throw e
+    }
+}
+
+
+if (!CHART_DEF) {
+    throw 'function CHART_DEF not found or is nullish!'
+}
+if (!CHART_DATA) {
+    throw 'variable CHART_DATA not found or is nullish!'
+}
 // 通过输出流返回结果SVG字符串
-console.log(renderSvgStr(width, height, option))
+console.log(renderSvgStr(width, height, CHART_DEF(CHART_DATA)))
